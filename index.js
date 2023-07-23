55ec6980001d57675219cc4737fb8c6f4e96153d
studentAbuSayeed <studentabusayeed2@gmail.com>
2023-07-23 18:11:21 +0600

    put method added

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
